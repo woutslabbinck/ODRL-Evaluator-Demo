c72b2c4c1d6e2f2b12f11135de3e803ee1ddcb59
woutslabbinck <wout.slabbinck@ugent.be>
2025-03-06 14:58:09 +0100

    feat: rename dist to docs

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Checkout the live demo [here](https://woutslabbinck.github.io/ODRL-Evaluator-Dem
 
 ## Develop locally
 
-The bulk of the code is in the [index.html](./dist/index.html) file.
+The bulk of the code is in the [index.html](./docs/index.html) file.
 ```sh
 npm start
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
     },
     devServer: {
         static: {
-            directory: path.join(__dirname, "dist"),
+            directory: path.join(__dirname, "docs"),
           },
         compress: true,
         port: 8080
